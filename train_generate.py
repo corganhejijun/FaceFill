8f37a25ecd05c962698476d75e6652de0ee3b4e1
corganhejijun <corganhejijun@gmail.com>
2018-11-27 10:30:32 +0800

    train sample with bleed

--- train_generate.py
+++ train_generate.py
@@ -8,4 +8,4 @@ SHAPE_MODEL = "models/shape_predictor_68_face_landmarks.dat"
 IMG_SIZE = 128
 FACE_SIZE = 64
 
-headFromDir(IN_DIR, OUT_DIR, SHAPE_MODEL, IMG_SIZE, FACE_SIZE)
+headFromDir(IN_DIR, OUT_DIR, SHAPE_MODEL, IMG_SIZE, FACE_SIZE, 10, 30)
